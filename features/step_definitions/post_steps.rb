2289421c0e2868fc099625628d5f52766bc4a8c5
Jiazhen Chen <jiazhen.chen@berkeley.edu>
2017-11-05 22:15:28 +0000

    [test] added cucumber test for edit_post feature, funcs not implemented

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -1,16 +1,13 @@
 When /^(?:|I )select "(.*)" from "(.*)"$/ do |value, field|
   select(value, :from => field)
-  # PostsController.update_form_subcategory
 end
-
-# When /^(?:|I )select "(.*)" from subcategory$/ do |value|
-  
-#   page.find_by_id('post_subcategory').find("option[value='book']").select_option
-#     # select value, :from => field
-# end
 
 Then /^(?:|I )should see "(.*)" in the field "(.*)"$/ do |val, field|
     expect(find_field(field).value).to eq val
+end
+
+Then /^(?:|I )should not see "(.*)" in the field "(.*)"$/ do |val, field|
+    expect(find_field(field).value).not_to eq val
 end
 
 Then /^(?:|I )attach the file to "(.*)"$/ do |field|
