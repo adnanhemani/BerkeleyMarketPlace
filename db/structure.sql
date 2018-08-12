666f34490b53969ca634be333d69fd99802944ad
EC2 Default User <ec2-user@ip-172-31-38-93.us-east-2.compute.internal>
2018-08-12 09:53:53 +0000

    postgres

--- db/structure.sql
+++ db/structure.sql
@@ -3,7 +3,6 @@
 --
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
@@ -272,8 +271,6 @@ INSERT INTO schema_migrations (version) VALUES ('20171028212304');
 INSERT INTO schema_migrations (version) VALUES ('20171105194148');
 
 INSERT INTO schema_migrations (version) VALUES ('20171112180401');
-
-INSERT INTO schema_migrations (version) VALUES ('20171116213035');
 
 INSERT INTO schema_migrations (version) VALUES ('20171116214206');
 
